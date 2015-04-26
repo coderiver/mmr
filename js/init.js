5830301fa7395803ef1069d95c75407da929dee9
whats0n <mrdvv@mail.ru>
2015-04-26 18:34:26 +0300

    done

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
 					"js/lib/chosen.jquery.js",
-					"js/lib/jquery.plugin.js",
-					"js/lib/jquery.datepick.js",
+					"js/lib/jquery-ui.min.js",
 					"js/common.js");
